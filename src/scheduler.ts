2861f56881d662afea64e9e8c0409e7979dbdc93
pick-cee <akinloluwaolumuyide@gmail.com>
2024-06-13 16:02:06 +0100

    updates

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -1,7 +1,7 @@
 import { Injectable, Logger } from "@nestjs/common";
 import { InjectRepository } from "@nestjs/typeorm";
-import { MEMBERSHIP_TYPE, Membership } from "./schemas";
-import { Between, Repository } from "typeorm";
+import { Membership } from "./schemas";
+import { Repository } from "typeorm";
 import { EmailSevice } from "./email.service";
 import { AppService } from "./app.service";
 import { Cron, CronExpression } from "@nestjs/schedule";
@@ -80,76 +80,5 @@ export class ScheduleService {
                 });
             }
         });
-
-        // const today = new Date()
-        // const sevenDaysFromNow = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
-        // console.log(sevenDaysFromNow)
-
-        // // Query for annual memberships due in 7 days
-        // const upcomingAnnualMemberships = await this.membershipRepo.find({
-        //     where: {
-        //         membershipType: MEMBERSHIP_TYPE.ANNUAL_BASIC,
-        //         dueDate: Between(today, sevenDaysFromNow)
-        //     },
-        //     relations: ['member']
-        // })
-
-        // // query for monthly memberships due this month
-        // const currentMonthStart = new Date(today.getFullYear(), today.getMonth(), 1);
-        // const currentMonthEnd = new Date(today.getFullYear(), today.getMonth() + 1, 0);
-        // console.log(currentMonthStart, currentMonthEnd)
-        // const upcomingMonthlyMemberships = await this.membershipRepo.find({
-        //     where: {
-        //         membershipType: MEMBERSHIP_TYPE.MONTHLY_PREMIUM,
-        //         monthlyDueDate: Between(currentMonthStart, currentMonthEnd),
-        //     },
-        //     relations: ['member'],
-        // });
-
-        // // send remiders for annual memberships
-        // for (const membership of upcomingAnnualMemberships) {
-        //     const invoice = await this.invoiceSvc.generateInvoice(membership.id)
-        //     await this.emailSvc.sendAnnualEmail(
-        //         {
-        //             dueDate: membership.dueDate,
-        //             email: membership.member.email,
-        //             firstName: membership.member.firstName,
-        //             lastName: membership.member.lastName,
-        //             totalAmount: invoice.amount,
-        //             invoice: invoice,
-        //             mebershipType: membership.membershipType
-        //         }
-        //     )
-        // }
-
-        // // send reminders for monthly memberships
-        // for (const membership of upcomingMonthlyMemberships) {
-        //     const invoice = await this.invoiceSvc.generateInvoice(membership.id)
-        //     if (membership.isFirstMonth) {
-        //         await this.emailSvc.sendFirstMonthRemider(
-        // {
-        //     dueAmount: invoice.amount,
-        //     email: membership.member.email,
-        //     firstName: membership.member.firstName,
-        //     lastName: membership.member.lastName,
-        //     invoice: invoice,
-        //     membershipType: membership.membershipType,
-        // }
-        //         )
-        //     }
-        //     else {
-        //         await this.emailSvc.sendMonthlyEmail(
-        //             {
-        //                 dueDate: membership.dueDate,
-        //                 email: membership.member.email,
-        //                 firstName: membership.member.firstName,
-        //                 lastName: membership.member.lastName,
-        //                 invoice: invoice,
-        //                 membershipType: membership.membershipType,
-        //                 monthlyAmount: membership.monthlyAmount
-        //             }
-        //         )
-        //     }
-        // }
     }
 }
